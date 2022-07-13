038642306f0c249c3b6cc9f377bb125a3f9142bd
Yafen Fang <fangyaf@spaceml1.ethz.ch>
2022-07-13 11:42:29 +0200

    cifar: epoch 20 in scripts

--- examples/cifar/run_main_time.sh
+++ examples/cifar/run_main_time.sh
@@ -2,9 +2,9 @@
 set -e
 export CUDA_VISIBLE_DEVICES=0,1,2,5
 
-# python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm gradient_allreduce --lr 0.05 --epochs 50 > /dev/null
-python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm test --lr 0.05 --epochs 50 > /dev/null
-python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm sparsepy-simple --lr 0.05 --epochs 50 > /dev/null
-python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm sparsepy --lr 0.05 --epochs 50 > /dev/null
-python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm sparsepy-allgather --lr 0.05 --epochs 50 > /dev/null
-python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm sparsepy-allgather-full --lr 0.05 --epochs 50 > /dev/null
+python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm gradient_allreduce --lr 0.05 --epochs 20 > /dev/null
+python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm test --lr 0.05 --epochs 20 > /dev/null
+python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm sparsepy-simple --lr 0.05 --epochs 20 > /dev/null
+python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm sparsepy --lr 0.05 --epochs 20 > /dev/null
+python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm sparsepy-allgather --lr 0.05 --epochs 20 > /dev/null
+python -m bagua.distributed.launch --nproc_per_node=4 main_time.py --algorithm sparsepy-allgather-full --lr 0.05 --epochs 20 > /dev/null
